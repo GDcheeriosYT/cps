1e41e470634bba4e6385e5fac13ed0bb0fa2705b
GDcheerios <54640414+GDcheeriosYT@users.noreply.github.com>
2021-09-01 12:56:20 -0500

    Create PA13Messerschmidt.java

---
+++ unit 1/src/PA13Messerschmidt.java
@@ -0,0 +1,18 @@
+
+public class gravity1_3 {
+
+	public static void main(String[] args) {
+		
+		int time = 10;
+		
+		final double gravity = 9.8;
+		
+		double height = 0.5 * gravity * time * time;
+		
+		double velocity = gravity * time;
+		
+		System.out.println("your looking at about a height of " + height + "m and are traveling at " + velocity + "mps");
+
+	}
+
+}
