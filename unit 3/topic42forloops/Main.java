d4d2da7d3c4b95eea8fee37a7dbe28d8708c7365
GDcheerios <54640414+GDcheeriosYT@users.noreply.github.com>
2021-11-05 13:30:15 -0500

    started 44

--- unit 3/topic42forloops/Main.java
+++ unit 3/topic42forloops/Main.java
@@ -1,34 +1,55 @@
-public class Main {
-    public static void main(String[] args) {
-        //for (spot1; spot2; spot3)
-        //spot1: delcraing/initializing an index variable
+// public class Main {
+//     public static void main(String[] args) {
+//         //for (spot1; spot2; spot3)
+//         //spot1: delcraing/initializing an index variable
 
-        //spot2: the loop condition. this loop runs WHILE this is true
+//         //spot2: the loop condition. this loop runs WHILE this is true
 
-        //spot3: updating the index variable
+//         //spot3: updating the index variable
 
+//         System.out.println(replace("himynameisjooeandimveryhoongry", "oo", "u"));
 
-        int i;
-        for (i = 0; i < 1000; i++) {
-            System.out.println(i);
+//         int i;
+//         for (i = 0; i < 1000; i++) {
+//             System.out.println(i);
             
-        }
+//         }
 
-        for (i = 0; i != 55; i+=5) {
-            System.out.println(i);
+//         for (i = 0; i != 55; i+=5) {
+//             System.out.println(i);
 
-        }
+//         }
 
-        for (i = 90; i != 68; i-=2) {
-            System.out.println(i);
+//         for (i = 90; i != 68; i-=2) {
+//             System.out.println(i);
 
-        }
+//         }
 
-        String str = "whack-a-mole";
+//         String str = "whack-a-mole";
 
-        for (i = 0; i <= str.length(); i++){
-            System.out.println(str.substring(i, i+1));
-        }
+//         for (i = 0; i <= str.length(); i++){
+//             System.out.println(str.substring(i, i+1));
+//         }
 
-    }
-}
+//     }
+
+//         public static String replace(String word, String search, String replace){
+
+//         String wordConstruction = "";
+//         String newWord = "";
+//         String partToAdd= "";
+
+//             for(int i = 0; i <= word.length(); i++){
+//                 if (word.substring(0, i).indexOf(search) != -1){
+//                     partToAdd = word.substring(word.substring(word.indexOf(search), i));
+//                     wordConstruction = word.substring(0, i) + PartToAdd;
+                    
+//             }
+//                 wordConstruction = "" + word.substring(0, i);
+//             }
+
+//         return wordConstruction;
+
+//         }
+  
+// }
